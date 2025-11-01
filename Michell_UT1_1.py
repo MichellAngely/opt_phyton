781ae03c9c5c7f6dcb19a7afec950be95a52835b
Michell Angely <micgarfer2@alu.edu.gva.es>
2025-11-01 18:20:21 +0100

    Add files via upload

---
+++ Michell_UT1_1.py
@@ -0,0 +1,12 @@
+
+# Michell Angely G
+
+Leche = 3.40
+Azúcar = 7.30
+Café = 4.40
+
+Total = Leche + Azúcar + Café
+Precio_Medio = Total / 3
+
+print("El total de su compra es:", Total, "$")
+print("El promedio de su compra es:", round(Precio_Medio, 2), "$")
