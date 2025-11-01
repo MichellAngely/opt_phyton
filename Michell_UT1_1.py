cc300d967d67766b72898303c39d800958af1660
Michell Angely <micgarfer2@alu.edu.gva.es>
2025-11-01 18:54:59 +0100

    Add exercises and calculations to Michell_UT1_1.py

--- Michell_UT1_1.py
+++ Michell_UT1_1.py
@@ -1,6 +1,5 @@
-
-# Michell Angely G
-
+# Michell Angely G Tarea 1
+# Ejercicio 1
 Leche = 3.40
 Azúcar = 7.30
 Café = 4.40
@@ -10,3 +9,18 @@ Precio_Medio = Total / 3
 
 print("El total de su compra es:", Total, "$")
 print("El promedio de su compra es:", round(Precio_Medio, 2), "$")
+
+#Ejercicio 2
+Precio_Cena = 45.00
+Propina= Precio_Cena * 0.10
+print("La propina es:", round(Propina, 2,), "$")
+
+#Ejercicio 3
+Altura_M = 1.60
+Altura_cm = Altura_M * 100
+print("Mi altura es:", Altura_cm, "cm")
+
+#Ejercicio 4
+MiEdad_Actual = 18
+MiEdad_2030 = MiEdad_Actual + (2030 - 2025)
+print("Mi edad en el año 2030 será:", MiEdad_2030)
