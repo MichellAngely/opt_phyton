9b26dcc408e51531915cfa50e2e8752ca87ce2e3
Michell Angely <micgarfer2@alu.edu.gva.es>
2025-11-01 19:28:50 +0100

    Add exercises and personal information in Python script

---
+++ Michell_UT1_2.py
@@ -0,0 +1,46 @@
+# Michell Angely G
+# Ejercicio 5
+
+#Variables:
+minombre = "Michell"
+miedad = 18
+
+#Mi frase:
+print(f"Holaa, soy {minombre} y tengo {miedad} añitos :D")
+
+#Ejercicio 6
+
+#Variable con frase:
+Prueba = "Estoy aprendiendo un lenguaje de programación"
+
+#Número de caracteres
+Cantidad = len(Prueba)
+
+print(f"La frase tiene un total de {Cantidad} caracteres.")
+
+#Ejercicio 7
+
+#Variables:
+sonido1 = "BOOM"
+sonido2 = ("CLAP")
+
+#Mi canción:
+
+cancion = (sonido1 + " " + sonido1 + " " + sonido1 +" " + "Gotta_get_that" + " " + sonido2 + "!")
+print("Esta es mi canción con referencia The Black Eyed Peas Boom Boom Pow")
+print(cancion)
+
+#Ejercicio 8
+
+#Variables:
+nombre = "Michell"
+edad = 18
+curso = "Python"
+nota_media = 7.5
+
+# La ficha:
+print("-_-_-_- Ficha de Alumno -_-_-_-")
+print(f"Nombre: {nombre}")
+print(f"Edad: {edad}")
+print(f"Curso: {curso}")
+print(f"Nota_media: {nota_media}")
