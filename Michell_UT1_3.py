f7602d4ef8a7e92f5e095936fedc98c0dfc16fd0
Michell Angely <micgarfer2@alu.edu.gva.es>
2025-11-01 20:06:03 +0100

    Create Michell_UT1_3.py

---
+++ Michell_UT1_3.py
@@ -0,0 +1,35 @@
+#Michell Angely G
+#Ejercicio 9
+
+preguntanombre = input("¿Cuál es tu nombre? ")
+print(f"Holaa {preguntanombre} Bienvenida!")
+
+#Ejercicio 10
+edad_usuario_actual = int(input("¿Cuál es tu edad?"))
+edad2030 = edad_usuario_actual + (2030 - 2025)
+
+print(f"Su edad en el 2030 será:", edad2030)
+
+#Ejercicio 11
+#Elección
+num1 = float(input("Introduce un numero: "))
+num2 = float(input("Introduce otro numero: "))
+
+#Calculo
+print("Suma de tus números")
+print(num1 + num2)
+
+print("Resta de tus numeros")
+print(num1 - num2)
+
+print("Multiplicación de tus números")
+print(num1 * num2)
+
+print("División de tus números")
+print(num1 / num2)
+
+#Ejercicio 12
+
+numero = float(input("Inserta un número"))
+print(f"El doble de {numero} es {numero * 2}.")
+print(f"El triple de {numero} es {numero * 3}.")
